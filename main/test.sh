84108e49ca477e41558b916885e03a095c34e632
李振邦 <lizhenbang56@163.com>
2021-01-05 16:29:52 +0800

    使用脚本同时进行测试和评估

--- main/test.sh
+++ main/test.sh
@@ -1,5 +1,5 @@
 dataset_name="GOT-10k_Val"
-loop_num=8192
+loop_num=2048
 do_attack=true
 
 if [ "$dataset_name" = GOT-10k_Val ];then
@@ -7,5 +7,7 @@ cfg=experiments/siamfcpp/test/got10k/siamfcpp_googlenet-got.yaml
 fi
 
 source activate pytorch1.4_python3.7
+export PYTHONPATH=/home/etvuz/projects/adversarial_attack/video_analyst:$PYTHONPATH
 cd /home/etvuz/projects/adversarial_attack/video_analyst
 python main/test.py --config $cfg --dataset_name=$dataset_name --do_attack=$do_attack --loop_num=$loop_num
+python main/eval.py --dataset_name=$dataset_name --loop_num=$loop_num
